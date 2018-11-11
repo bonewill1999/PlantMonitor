f650a4374c4738005fb5b604e1cf6c13a412af8f
bonewill <william.bone@mail.utoronto.ca>
2018-11-11 01:21:22 -0500

    Updated newuser

--- PlantMonitor/app/src/main/java/com/example/johnnybahama/plantmonitor/WelcomeScreen.java
+++ PlantMonitor/app/src/main/java/com/example/johnnybahama/plantmonitor/WelcomeScreen.java
@@ -1,9 +1,13 @@
 package com.example.johnnybahama.plantmonitor;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
 
 public class WelcomeScreen extends AppCompatActivity {
+
+    private ArrayList<Profile> profiles = new ArrayList<Profile>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- PlantMonitor/app/src/main/java/com/example/johnnybahama/plantmonitor/newuser.java
+++ PlantMonitor/app/src/main/java/com/example/johnnybahama/plantmonitor/newuser.java
@@ -6,19 +6,30 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 
+import java.security.Provider;
+
 public class newuser extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_newuser);
 
         Button navtomain = (Button) findViewById(R.id.create);
 
         navtomain.setOnClickListener(new View.OnClickListener() {
+
+            Profile profileHolder;
             @Override
             public void onClick(View view) {
+
+
+
+                //profileHolder.setEmail();
+
                 Intent intent = new Intent(newuser.this, WelcomeScreen.class);
                 startActivity(intent);
             }
